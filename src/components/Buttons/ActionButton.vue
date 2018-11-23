3ee66bbcb615e5f6874cd2530b25c1f93d21db56
Muhammed Nuhman <nuhman7@outlook.com>
2018-11-23 01:42:07 -0500

    add basic expense screen
    Add form to enter expense. Create corresponding components.

---
+++ src/components/Buttons/ActionButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class="btn btn-primary">{{ btnText }}</button>
+</template>
+
+<script>
+export default {
+  name: "ActionButton",
+  props: ["btnText"],
+  data() {
+    return {};
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+button:hover {
+  cursor: pointer;
+}
+</style>
---
+++ src/components/Inputs/BasicInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class=" row">
+    <label for="expenseDetails" class="col-sm-2 col-form-label">{{
+      labelTxt
+    }}</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" :placeholder="placeholderTxt" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["placeholderTxt", "labelTxt"],
+  name: "BasicInput",
+  data() {
+    return {
+      btnText: "ADD"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
